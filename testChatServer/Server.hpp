06af68237c0735541e7910ee798f79cd118cdd77
inhyuk15 <15inhyuk@gmail.com>
2023-08-29 03:26:16 +0900

    server 코루틴으로 수정

--- testChatServer/Server.hpp
+++ testChatServer/Server.hpp
@@ -2,18 +2,24 @@
 #define Server_hpp
 
 #include <boost/asio.hpp>
+
 #include <cstdlib>
 #include <iostream>
 #include <set>
 
+
 using boost::asio::ip::tcp;
+using boost::asio::awaitable;
+using boost::asio::co_spawn;
+using boost::asio::detached;
+using boost::asio::use_awaitable;
 
 class Room;
 
 class Server : public std::enable_shared_from_this<Server> {
 	 public:
 	Server(boost::asio::io_context& io_context, tcp::endpoint& endpoint);
-	void accept();
+	boost::asio::awaitable<void> accept();
 
 	 private:
 		tcp::acceptor acceptor_;
--- testChatServer/main.cpp
+++ testChatServer/main.cpp
@@ -11,11 +11,13 @@ int main(int argc, const char* argv[]) {
     std::string port = "4000";
     boost::asio::io_context io_context;
     tcp::endpoint endpoint(tcp::v4(), stoi(port));
+    try {
+        auto server = std::make_shared<Server>(io_context, endpoint);
 
-    auto server = std::make_shared<Server>(io_context, endpoint);
-    server->accept();
-
-    io_context.run();
+        io_context.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception in main " << e.what() << std::endl;
+    }
 
     return 0;
 }
